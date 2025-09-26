db912032e5c4fa58f540a20bd6b6e26bc39264b1
Vinothini <vinoism1703@gmail.com>
2025-09-26 09:44:40 +0000

    final

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,15 @@
-
 'use client';
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVmRQCGVHAlSd_GQsOGq43w_eAgrvPro4",
-  authDomain: "adroit-cortex-448707-n5.firebaseapp.com",
-  projectId: "adroit-cortex-448707-n5",
-  storageBucket: "adroit-cortex-448707-n5.firebasestorage.app",
-  messagingSenderId: "618931367385",
-  appId: "1:618931367385:web:21d9aebf554a161281bdc8"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
