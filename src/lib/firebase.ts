7f6d75c74b452f9ebc1b42bb0c4f3437905eca25
Vinothini <vinoism1703@gmail.com>
2025-09-26 08:08:33 +0000

    what need to be done now

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDe4_dIGBlOmP0s6N-YOwEWCYrlKBmCqf0",
-    authDomain: "kart-i-quo-financial-guide.firebaseapp.com",
-    projectId: "kart-i-quo-financial-guide",
-    storageBucket: "kart-i-quo-financial-guide.appspot.com",
-    messagingSenderId: "389651574924",
-    appId: "1:389651574924:web:e7c31d999e4f2666877995",
-    measurementId: "G-SQ05TDD2R6"
+    apiKey: "AIzaSyCVfVjD-7pW0M-CoDS4d_1sU_Lafdsg6J4",
+    authDomain: "kart-i-quo-financial-guide-502a1.firebaseapp.com",
+    projectId: "kart-i-quo-financial-guide-502a1",
+    storageBucket: "kart-i-quo-financial-guide-502a1.appspot.com",
+    messagingSenderId: "367352358872",
+    appId: "1:367352358872:web:60a92795c65a95f2e87c80",
+    measurementId: "G-8Z0B3S6E31"
 };
 
 // Initialize Firebase
